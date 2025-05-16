12db7665dee26b73483826d68ba6f63305f1c069
Siebe-Timmer <150017535+Siebe-Timmer@users.noreply.github.com>
2025-05-16 21:49:03 +0200

    class diagram

--- BrickBreaker/src/main/java/org/example/entities/TimerText.java
+++ BrickBreaker/src/main/java/org/example/entities/TimerText.java
@@ -7,34 +7,9 @@ import javafx.scene.text.Font;
 
 public class TimerText extends TextEntity {
 
-    private int secondsPassed = 0;
-
     public TimerText(Coordinate2D initialLocation) {
         super(initialLocation, "Time: 0s");
         setFill(Color.WHITE);
         setFont(Font.font("Arial", 24));
-    }
-
-    /**
-     * Roep deze methode aan om de timer met 1 seconde te verhogen en tekst aan te passen.
-     */
-    public void incrementTime() {
-        secondsPassed++;
-        setText("Time: " + secondsPassed + "s");
-    }
-
-    /**
-     * Optioneel: reset de timer terug naar 0.
-     */
-    public void reset() {
-        secondsPassed = 0;
-        setText("Time: 0s");
-    }
-
-    /**
-     * Geeft het aantal seconden terug dat verstreken is.
-     */
-    public int getSecondsPassed() {
-        return secondsPassed;
     }
 }
