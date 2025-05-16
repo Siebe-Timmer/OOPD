dbc8b5090b3ec17f063340965b96ddc559b4656f
Siebe-Timmer <150017535+Siebe-Timmer@users.noreply.github.com>
2025-05-16 10:44:50 +0200

    Collision met bal.

--- BrickBreaker/src/main/java/org/example/entities/Ball.java
+++ BrickBreaker/src/main/java/org/example/entities/Ball.java
@@ -3,13 +3,15 @@ package org.example.entities;
 import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.entities.Collided;
 import com.github.hanyaeger.api.entities.Collider;
+import com.github.hanyaeger.api.entities.SceneBorderTouchingWatcher;
 import com.github.hanyaeger.api.entities.impl.DynamicCircleEntity;
+import com.github.hanyaeger.api.scenes.SceneBorder;
 import javafx.scene.paint.Color;
 import org.example.entities.bricks.Brick;
 
 import java.util.List;
 
-public class Ball extends DynamicCircleEntity implements Collider, Collided {
+public class Ball extends DynamicCircleEntity implements Collider, Collided, SceneBorderTouchingWatcher {
 
     private Paddle paddle;
 
@@ -26,7 +28,7 @@ public class Ball extends DynamicCircleEntity implements Collider, Collided {
     public void onCollision(List<Collider> colliders) {
         for (Collider c : colliders) {
             if (c instanceof Paddle paddle) {
-                double paddleWidth = 100; // of paddle.getWidth() als dat werkt
+                double paddleWidth = 100;
                 double paddleCenterX = paddle.getAnchorLocation().getX();
                 double ballCenterX = getAnchorLocation().getX();
 
@@ -38,11 +40,58 @@ public class Ball extends DynamicCircleEntity implements Collider, Collided {
                 double angle = minAngle + (maxAngle - minAngle) * relativeImpact;
 
                 setMotion(getSpeed(), angle);
-            } else if (c instanceof Brick) {
-                // Voor nu: altijd rechtdoor omhoog
-                setMotion(getSpeed(), 0d);
+
+            } else if (c instanceof Brick brick) {
+
+                double ballX = getAnchorLocation().getX();
+                double ballY = getAnchorLocation().getY();
+                double brickX = brick.getAnchorLocation().getX();
+                double brickY = brick.getAnchorLocation().getY();
+
+                double dx = Math.abs(ballX - brickX);
+                double dy = Math.abs(ballY - brickY);
+
+                double halfWidth = 64 / 2.0;
+                double halfHeight = 32 / 2.0;
+
+                double overlapX = halfWidth - dx;
+                double overlapY = halfHeight - dy;
+
+                double direction = getDirection();
+                double newDirection;
+
+                if (overlapX < overlapY) {
+                    newDirection = (360 - direction) % 360;
+                } else {
+                    newDirection = (180 - direction + 360) % 360;
+                }
+
+                setDirection(newDirection);
+
+
             }
         }
+    }
+
+
+    @Override
+    public void notifyBoundaryTouching(SceneBorder sceneBorder) {
+        double direction = getDirection();
+
+        switch (sceneBorder) {
+            case LEFT:
+            case RIGHT:
+                setDirection((360 - direction) % 360);
+                break;
+
+            case TOP:
+                setDirection((180 - direction + 360) % 360);
+                break;
+            case BOTTOM:
+                remove();
+
+        }
+
     }
 
 }
